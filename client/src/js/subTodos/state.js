16db2358ebd8753629dadabd40ff079d7454baff
sofresh <sofreshian@gmail.com>
2021-02-02 18:57:02 +0900

    feat: subTodos default settings completed

---
+++ client/src/js/subTodos/state.js
@@ -0,0 +1,7 @@
+const todos = [
+  { id: 1, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: false },
+  { id: 3, content: 'Javascript', completed: false }
+];
+
+export default todos;

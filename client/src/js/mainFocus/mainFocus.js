d83b3e329c3a906a34a0e0afabdfe36aa2c6441f
bcround <sbc1142@gmail.com>
2021-02-03 13:07:10 +0900

    feat: Fix minute's text and Add name edit function

--- client/src/js/mainFocus/mainFocus.js
+++ client/src/js/mainFocus/mainFocus.js
@@ -1,5 +1,7 @@
 import renderTime from './renderTime';
+import nameEdit from './nameEdit';
 
 export default () => {
   renderTime();
+  nameEdit();
 };
--- client/src/js/mainFocus/renderTime.js
+++ client/src/js/mainFocus/renderTime.js
@@ -1,10 +1,25 @@
 export default () => {
   // DOM
   const $clock = document.querySelector('.clock');
+  const $greetingTime = document.querySelector('.greeting-time');
 
   setInterval(() => {
     const today = new Date();
 
-    $clock.textContent = `${today.getHours()}:${today.getMinutes()}`;
+    if (today.getHours() < 12 && today.getMinutes() <= 59) {
+      $greetingTime.textContent = ' Morning ';
+    } else if (
+      today.getHours() >= 12 &&
+      today.getHours() < 18 &&
+      today.getMinutes() <= 59
+    ) {
+      $greetingTime.textContent = ' Afternoon ';
+    } else if (today.getHours() >= 18) {
+      $greetingTime.textContent = ' Evening ';
+    }
+
+    $clock.textContent = `${today.getHours()}:${
+      today.getMinutes() < 10 ? `0${today.getMinutes()}` : today.getMinutes()
+    }`;
   }, 1000);
 };

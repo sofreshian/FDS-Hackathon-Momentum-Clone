6f0932e4dd34a21da17d94340c5045e28cc9c707
bcround <sbc1142@gmail.com>
2021-02-03 10:35:30 +0900

    feat: Change some code so that it can show real time

--- client/src/js/mainFocus/renderTime.js
+++ client/src/js/mainFocus/renderTime.js
@@ -2,9 +2,9 @@ export default () => {
   // DOM
   const $clock = document.querySelector('.clock');
 
-  const today = new Date();
-
   setInterval(() => {
+    const today = new Date();
+
     $clock.textContent = `${today.getHours()}:${today.getMinutes()}`;
   }, 1000);
 };

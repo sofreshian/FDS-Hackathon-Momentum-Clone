55d472c074ba6a12f892522c3645da2e68ce49bf
bcround <sbc1142@gmail.com>
2021-02-04 09:48:36 +0900

    feat: Complete toggle main focus and add styling to it

--- client/src/js/mainFocus/mainFocus.js
+++ client/src/js/mainFocus/mainFocus.js
@@ -1,9 +1,11 @@
 import renderTime from './renderTime';
 import fetchMainFocus from './fetchMainFocus';
 import addMainFocus from './addMainFocus';
+import toggleMainFocus from './toggleMainFocus';
 
 export default () => {
   renderTime();
   fetchMainFocus();
   addMainFocus();
+  toggleMainFocus();
 };

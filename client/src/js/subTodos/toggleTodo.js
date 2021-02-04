b297b2d0caff1def61a8bd511973ca489e64ae9b
sofresh <sofreshian@gmail.com>
2021-02-04 11:49:45 +0900

    feat: toggleTodo updated

--- client/src/js/subTodos/toggleTodo.js
+++ client/src/js/subTodos/toggleTodo.js
@@ -2,18 +2,15 @@ import axios from 'axios';
 import render from './render';
 // import state from './todoState';
 
-const state = axios
-  .get('http://localhost:8000/todos')
-  .then(({ data: _todos }) => _todos);
 const $todoListContent = document.querySelector('.todo-list-content');
 
 export default () => {
   const toggleTodo = async id => {
+    const res = await axios.get('http://localhost:8000/todos');
+    const state = res.data;
     const completed = !state.find(todo => todo.id === +id).completed;
-    axios
-      .patch(`http://localhost:8000/todos/${id}`, { completed })
-      .then(render)
-      .catch(console.error);
+    const resTwo = await axios.patch(`http://localhost:8000/todos/${id}`, { completed });
+    render(resTwo.data);
   };
 
   $todoListContent.onchange = e => {

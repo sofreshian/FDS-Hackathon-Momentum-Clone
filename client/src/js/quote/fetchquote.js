ed2ed4a7daa64e04e8defe56d24fa2bac9ca7204
younho <nnyyhh9247@gmail.com>
2021-02-04 10:35:13 +0900

    complete quote

---
+++ client/src/js/quote/fetchquote.js
@@ -0,0 +1,12 @@
+import renderQuote from './renderquote';
+
+const getQuote = async () => {
+  try {
+    const res = await axios.get('https://goquotes-api.herokuapp.com/api/v1/random?count=30')
+    renderQuote(res.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default getQuote;
---
+++ client/src/js/quote/quote.js
@@ -0,0 +1,5 @@
+import getQuote from './fetchquote';
+
+export default () => {
+  getQuote();
+};
